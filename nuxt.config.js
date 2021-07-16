b9383625cdae6cbb84ed3ec02f8f078ee11dc1ba
Jose Vega <jocvegar@gmail.com>
2021-07-15 19:20:31 -0600

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,85 @@
+import colors from "vuetify/es5/util/colors";
+var FriendlyErrorsWebpackPlugin = require("@nuxtjs/friendly-errors-webpack-plugin");
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: "%s - ecommerce",
+    title: "ShopMe",
+    htmlAttrs: {
+      lang: "en"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    link: [
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap"
+      }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    "@nuxtjs/vuetify"
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    "@nuxtjs/axios"
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    treeShake: true,
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      themes: {
+        light: {
+          primary: "#2D3753",
+          secondary: "#2D3753",
+          accent: "#3AE3D6",
+          error: "#f44336",
+          info: "#2d3753",
+          success: "#7a904a",
+          warning: "#f78012"
+        },
+        dark: {
+          primary: colors.blue.lighten3,
+          background: colors.indigo.base
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    friendlyErrors: true
+  },
+
+  webpackConfig: {
+    plugins: [new FriendlyErrorsWebpackPlugin()]
+  }
+};
